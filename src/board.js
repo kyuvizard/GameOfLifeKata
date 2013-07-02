a491eac9bd9f6aceabc93ade5b3af9c0f4c3f7fb
Erik Romero <edragon_rocker@hotmail.com>
2013-07-02 01:50:07 -0400

    Fixed all methods Added updateBoard method

--- src/board.js
+++ src/board.js
@@ -7,100 +7,80 @@ var Cell = (function(){
 
 })();
 
-
 var Board = (function(){
 	return function(height,width){
 
-		var height = 5;
+		var height = 6;
 		var width = 5;
 
 		var arrayCell = new Array();
 		var _grid = new Array(height);
-
-		var fillGrid = function(){
-
-			for (var i = 0; i < this.height; i++) {
-				_grid[i] = new Array(width);
-				for (var ind = 0; ind < this.width; ind++) {
-					_grid[i][ind] = null;
-				}
-			}
-		}
+		for (var i = 0; i < height; i++)
+			_grid[i] = new Array(width);
 
 
 		var fillLiveCell = function(){
-			for (var i = 0; i < height; i++) {
-				for (var j = 0; j < width; j++) {
-					if(_grid[i][j]!=null )	
-						arrayCell.push( _grid[i][j]);
-				};
-			};
-			/*console.log(_grid[1][1]);
-			arrayCell[1][1] = 0;			
-			console.log(_grid[1][1])*/
+			for (var h = 0; h < height; h++) {
+				for (var w = 0; w < width; w++) {
+					if(_grid[w][h] )
+						arrayCell.push( new Cell(w, h));
+				}
+			}
+		}
+
+		var updateBoard= function(){
+			for (var i = 0; i < arrayCell.length; i++) {
+				var x = arrayCell[i].x;
+				var y = arrayCell[i].y;
+
+				_grid[x][y].isLife = arrayCell[i].isLife;
+			}
 		}
 
 		var fillCell = function(){
-			
-			for (var i = 0; i < height; i++) {
-				_grid[i] = new Array(width);
-				for (var ind = 0; ind < width; ind++) {
-					_grid[i][ind] = null;
-				}
-			}
 			for (var i = 0; i < 10; i++) {
 				var h = Math.floor((Math.random()*height-1));
 				var w = Math.floor((Math.random()*width-1));
 				if(h<=0) h = 1;
 				if(w<=0) w = 1;
 				_grid[w][h] = new Cell(w,h);
-
-				
 			};
 			fillLiveCell();
 		}
 
 		this.drawBoard = function(){
-
-			fillGrid();
 			fillCell();
 			this.iterate();
 		}
 
-
 		this.viewNextyGenera = function(){
-
-
 			for (var i = 0; i < arrayCell.length; i++) {
 				var x =arrayCell[i].x;
 				var y =arrayCell[i].y;
 				var neighborsLive = 0;
 				var neighborsUnLive = 0;
 				if(arrayCell[i].isLife) {
-					if(_grid[x+1][y]!=null&&_grid[x+1][y].isLife)
+					if(_grid[x+1][y] &&_grid[x+1][y].isLife)
 						neighborsLive++;
-					if(_grid[x+1][y+1]!=null&&_grid[x+1][y+1].isLife)
+					if(_grid[x+1][y+1] && _grid[x+1][y+1].isLife)
 						neighborsLive++;
-					if(_grid[x+1][y-1]!=null&&_grid[x+1][y-1].isLife)
+					if(_grid[x+1][y-1] && _grid[x+1][y-1].isLife)
 						neighborsLive++;
-					if(_grid[x][y+1]!=null&&_grid[x][y+1].isLife)
+					if(_grid[x][y+1] && _grid[x][y+1].isLife)
 						neighborsLive++;
-					if(_grid[x][y-1]!=null&&_grid[x][y-1].isLife)
+					if(_grid[x][y-1] && _grid[x][y-1].isLife)
 						neighborsLive++;
-					if(_grid[x-1][y]!=null&&_grid[x-1][y].isLife)
-						neighborsLive++;	
-					if(_grid[x-1][y+1]!=null&&_grid[x-1][y+1].isLife)
+					if(_grid[x-1][y] && _grid[x-1][y].isLife)
 						neighborsLive++;
-					if(_grid[x-1][y-1]!=null&&_grid[x-1][y-1].isLife)
+					if(_grid[x-1][y+1] && _grid[x-1][y+1].isLife)
+						neighborsLive++;
+					if(_grid[x-1][y-1] && _grid[x-1][y-1].isLife)
 						neighborsLive++;
 					if(neighborsLive<2||neighborsLive>3)
 						arrayCell[i].isLife = false;
-
-					
 				}
-				
 			};
-
+			updateBoard();
 			this.iterate();
 			arrayCell = new Array();
 			fillLiveCell();
@@ -108,20 +88,19 @@ var Board = (function(){
 
 		this.iterate = function(){
 			var str = '';
-			for (var i = 0; i < height; i++) {
-				
-				for (var j = 0; j < width; j++) {
+			for (var h = 0; h < height; h++) {
 
-					//console.log( _grid[i][j]);
-					if(_grid[i][j] != null) {
-						
-						if(_grid[i][j].isLife)
-							str +='1';
+				for (var w = 0; w < width; w++) {
+
+					if(_grid[w][h] != null) {
+
+						if(_grid[w][h].isLife)
+							str +=' 1 ';
 						else
-							str +='0';
+							str +=' 0 ';
 					}
-					else 
-						str += 'N';
+					else
+						str += ' N ';
 				};
 				console.log(str);
 				str = '';
@@ -131,4 +110,4 @@ var Board = (function(){
 	};
 })();
 
-
+var g = new Board();
